42f9b461340e765c8152f960c6f2d69cd8c57c5a
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2021-09-13 22:48:32 +0200

    Show "Please log in first." tooltip when user hovers over a "Also looking for a solution for this problem" button when logged out.

--- src/SolutionRequest.js
+++ src/SolutionRequest.js
@@ -1,111 +1,138 @@
-import React, { useState, useCallback } from 'react'
-import './SolutionRequest.css'
+import React, { useCallback, useState } from 'react'
 import { requestASolutionToo } from './domain/requestSolutionToo.js'
 import { addSolutionRequestToUsersRequestedSolutions } from './firebase/addSolutionRequestToUsersRequestedSolutions.js'
 import { getDatabase } from './firebase/getDatabase.js'
 import { initializeApp } from './firebase/initializeApp.js'
+import './SolutionRequest.css'
+import { useIsLoggedIn } from './useIsLoggedIn.js'
 import { useUserSolutionRequests } from './useUserSolutionRequests.js'
+
+/* global bootstrap */
 
 initializeApp()
 
 const database = getDatabase()
 
-export function SolutionRequest({solutionRequest}) {
+export function SolutionRequest({ solutionRequest }) {
   const id = solutionRequest.id
   const requestData = solutionRequest.data()
-  const {summary, details} = requestData
+  const { summary, details } = requestData
   const title = summary
   const body = details
 
+  const isLoggedIn = useIsLoggedIn()
+
+  const initializePopover = useCallback(
+    node => {
+      if (node && !isLoggedIn) {
+        new bootstrap.Popover(
+          node,
+          {
+            trigger: 'hover focus',
+            content: 'Please log in first.'
+          }
+        )
+      }
+    },
+    [isLoggedIn]
+  )
+
   const [numberOfRequesters, setNumberOfRequesters] = useState(
-    requestData.numberOfRequesters
+    requestData.numberOfRequesters,
   )
 
   const userSolutionRequests = useUserSolutionRequests()
   const userSolutionRequestIds = userSolutionRequests ?
     new Set(
-      userSolutionRequests.map(userSolutionRequest => userSolutionRequest.id)
+      userSolutionRequests.map(userSolutionRequest => userSolutionRequest.id),
     ) :
     null
   const [
     isAlsoLookingForASolutionForThisProblemRequestInProgress,
-    setIsAlsoLookingForASolutionForThisProblemRequestInProgress
+    setIsAlsoLookingForASolutionForThisProblemRequestInProgress,
   ] = useState(false)
 
   const onAlsoLookingForASolutionForThisProblem = useCallback(
-      async function onAlsoLookingForASolutionForThisProblem() {
-        setIsAlsoLookingForASolutionForThisProblemRequestInProgress(true)
-        const newNumberOfRequesters = requestASolutionToo(numberOfRequesters)
-        setNumberOfRequesters(newNumberOfRequesters)
+    async function onAlsoLookingForASolutionForThisProblem() {
+      setIsAlsoLookingForASolutionForThisProblemRequestInProgress(true)
+      const newNumberOfRequesters = requestASolutionToo(numberOfRequesters)
+      setNumberOfRequesters(newNumberOfRequesters)
 
-        const solutionRequestReference = database
-          .collection('solutionRequests')
-          .doc(id)
-        try {
-          const newNumberOfRequestersFromDatabase = await database.runTransaction(
-            async (transaction) => {
-              const solutionRequest = await transaction.get(solutionRequestReference)
-              if (solutionRequest.exists) {
-                await addSolutionRequestToUsersRequestedSolutions(solutionRequest.id)
+      const solutionRequestReference = database
+        .collection('solutionRequests')
+        .doc(id)
+      try {
+        const newNumberOfRequestersFromDatabase = await database.runTransaction(
+          async (transaction) => {
+            const solutionRequest = await transaction.get(solutionRequestReference)
+            if (solutionRequest.exists) {
+              await addSolutionRequestToUsersRequestedSolutions(solutionRequest.id)
 
-                const newNumberOfRequesters2 = requestASolutionToo(
-                  solutionRequest.data().numberOfRequesters ?? 0
-                )
-                transaction.update(
-                  solutionRequestReference,
-                  {
-                    numberOfRequesters: newNumberOfRequesters2,
-                  }
-                )
-                return newNumberOfRequesters2
-              }
-            },
-          )
-          if (typeof newNumberOfRequestersFromDatabase === 'number') {
-            setNumberOfRequesters(newNumberOfRequestersFromDatabase)
-          }
-        } catch (error) {
-          console.error(error)
-        } finally {
-          setIsAlsoLookingForASolutionForThisProblemRequestInProgress(false)
+              const newNumberOfRequesters2 = requestASolutionToo(
+                solutionRequest.data().numberOfRequesters ?? 0,
+              )
+              transaction.update(
+                solutionRequestReference,
+                {
+                  numberOfRequesters: newNumberOfRequesters2,
+                },
+              )
+              return newNumberOfRequesters2
+            }
+          },
+        )
+        if (typeof newNumberOfRequestersFromDatabase === 'number') {
+          setNumberOfRequesters(newNumberOfRequestersFromDatabase)
         }
+      } catch (error) {
+        console.error(error)
+      } finally {
+        setIsAlsoLookingForASolutionForThisProblemRequestInProgress(false)
+      }
     },
     [
       id,
-      numberOfRequesters
-    ]
+      numberOfRequesters,
+    ],
   )
 
   return (
-    <div data-id={id} className="card mb-2">
+    <div data-id={ id } className="card mb-2">
       <div className="card-body">
         <span
           className="number-of-requesters float-end"
           title="Number of people looking for a solution for this problem"
         >
-          {numberOfRequesters}
+          { numberOfRequesters }
         </span>
-        <h5 className="card-title">{title}</h5>
+        <h5 className="card-title">{ title }</h5>
         <p>
-          {body}
+          { body }
         </p>
-        {/*<a href="#" className="card-link">Details</a>*/}
+        {/*<a href="#" className="card-link">Details</a>*/ }
         <div className="float-end">
           {/*
           <button className="btn btn-light propose-solution mb-2 mb-md-0 me-2">
             Propose solution
-          </button>*/}{/*
-        */}<button
-            className="btn btn-light also-looking-for-a-solution-for-this-problem"
-            disabled={
-              isAlsoLookingForASolutionForThisProblemRequestInProgress ||
-              !userSolutionRequestIds ||
-              userSolutionRequestIds.has(id)
-            }
-            onClick={onAlsoLookingForASolutionForThisProblem}
+          </button>*/ }{/*
+        */ }
+          <div
+            ref={initializePopover}
+            className="d-inline-block"
+            tabIndex="0"
           >
+            <button
+              className="btn btn-light also-looking-for-a-solution-for-this-problem"
+              disabled={
+                isAlsoLookingForASolutionForThisProblemRequestInProgress ||
+                !userSolutionRequestIds ||
+                userSolutionRequestIds.has(id)
+              }
+              onClick={ onAlsoLookingForASolutionForThisProblem }
+            >
               Also looking for a solution for this problem
-          </button>
+            </button>
+          </div>
         </div>
       </div>
     </div>
