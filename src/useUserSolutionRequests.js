87e9ee5552ff6585ff4f5cfe646d07aa6cec69f9
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2021-09-13 14:40:52 +0200

    Unmounting logic for useUserSolutionRequests

--- src/useUserSolutionRequests.js
+++ src/useUserSolutionRequests.js
@@ -1,25 +1,41 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 export function useUserSolutionRequests() {
   const [userSolutionRequests, setUserSolutionRequests] = useState([])
 
-  firebase.auth().onAuthStateChanged(async (user) => {
-    if (user) {
-      const userId = user.uid
-      const database = firebase.firestore()
-      const userSolutionRequestsRef = database.collection('users').doc(userId)
-        .collection('requests')
-      userSolutionRequestsRef.onSnapshot(snapshot => {
-        const solutionRequests = []
-        snapshot.forEach(document => {
-          solutionRequests.push(document)
-        })
-        setUserSolutionRequests(solutionRequests)
+  useEffect(
+    () => {
+      let hasBeenUnmounted = false
+      let unsubscribe = null
+
+      firebase.auth().onAuthStateChanged(async (user) => {
+        if (!hasBeenUnmounted && user) {
+          const userId = user.uid
+          const database = firebase.firestore()
+          const userSolutionRequestsRef = database.collection('users').doc(userId)
+            .collection('requests')
+          unsubscribe = userSolutionRequestsRef.onSnapshot(snapshot => {
+            if (!hasBeenUnmounted) {
+              const solutionRequests = []
+              snapshot.forEach(document => {
+                solutionRequests.push(document)
+              })
+              setUserSolutionRequests(solutionRequests)
+            }
+          })
+        }
       })
+
+      return () => {
+        hasBeenUnmounted = true
+        if (unsubscribe) {
+          unsubscribe()
+        }
+      }
     }
-  })
+  )
 
   return userSolutionRequests
 }
