176c4edd8de50ed4b7a1dded9e233361117c61e1
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2021-09-13 13:28:08 +0200

    Fixes SolutionRequests constantly rerendering

--- src/useRequests.js
+++ src/useRequests.js
@@ -1,19 +1,22 @@
 import firebase from 'firebase'
 import 'firebase/firestore'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 export function useRequests() {
   const [requests, setRequests] = useState([])
 
-  ;(async () => {
-    const database = firebase.firestore()
-    const solutionRequests = await database.collection('solutionRequests')
-      .get(
-        database.collection('solutionRequests')
-          .orderBy('numberOfRequesters', 'desc'),
-      )
-    setRequests(solutionRequests.docs)
-  })()
+  useEffect(
+    async () => {
+      const database = firebase.firestore()
+      const solutionRequests = await database.collection('solutionRequests')
+        .get(
+          database.collection('solutionRequests')
+            .orderBy('numberOfRequesters', 'desc'),
+        )
+      setRequests(solutionRequests.docs)
+    },
+    []
+  )
 
   return requests
 }
