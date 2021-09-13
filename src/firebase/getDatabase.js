86c6fcb5d7d5b87e6237dfad87f72e4347ea5276
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2021-09-13 14:52:54 +0200

    Fixes "multiple initializations of database" error

--- src/firebase/getDatabase.js
+++ src/firebase/getDatabase.js
@@ -1,10 +1,12 @@
 import firebase from 'firebase'
+import 'firebase/firestore'
 import { isDevelopment } from '../isDevelopment.js'
+import memoize from 'lodash.memoize'
 
-export function getDatabase() {
+export const getDatabase = memoize(function getDatabase() {
   const database = firebase.firestore()
   if (isDevelopment()) {
     database.useEmulator('localhost', 8080)
   }
   return database
-}
+})
