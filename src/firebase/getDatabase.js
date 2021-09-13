d8c614c67c9f5234ea440e49b5d8a478489b3a49
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2021-09-13 15:04:12 +0200

    A way to detect if the database has already been started

--- src/firebase/getDatabase.js
+++ src/firebase/getDatabase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 import { isDevelopment } from '../isDevelopment.js'
-import memoize from 'lodash.memoize'
+import { hasDatabaseAlreadyBeenStarted } from './hasDatabaseAlreadyBeenStarted.js'
 
-export const getDatabase = memoize(function getDatabase() {
+export const getDatabase = function getDatabase() {
   const database = firebase.firestore()
-  if (isDevelopment()) {
+  if (isDevelopment() && !hasDatabaseAlreadyBeenStarted(database)) {
     database.useEmulator('localhost', 8080)
   }
   return database
-})
+}
