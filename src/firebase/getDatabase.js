0219b879f3e90941a885412fa6d76ec759d1fccd
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2021-09-13 14:54:34 +0200

    More specific firebase import

--- src/firebase/getDatabase.js
+++ src/firebase/getDatabase.js
@@ -1,4 +1,4 @@
-import firebase from 'firebase'
+import firebase from 'firebase/app'
 import 'firebase/firestore'
 import { isDevelopment } from '../isDevelopment.js'
 import memoize from 'lodash.memoize'
