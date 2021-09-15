1bbe78c6f62e18c731d8e90d5b5620b3a8d97e47
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2021-09-15 20:11:31 +0200

    Spacing for log in and log out button on mobile

--- src/LogOut.js
+++ src/LogOut.js
@@ -5,7 +5,7 @@ export function LogOut() {
   return (
     <button
       onClick={logOut}
-      className="btn btn-outline-dark ms-2"
+      className="btn btn-outline-dark mt-3 mb-2 mt-lg-0 ms-lg-2 mb-lg-0"
       type="button"
     >
       Log out
