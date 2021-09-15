e514a8196132aaff0d555e770570679d722fc7fa
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2021-09-15 17:29:56 +0200

    Adds missing 's'

--- src/useSolutionProposals.js
+++ src/useSolutionProposals.js
@@ -1,11 +1,11 @@
 import { useMemo } from 'react'
 import { getSolutionProposalsCollection } from './getSolutionProposalsCollection.js'
-import { useDocumentSnapshot } from './unnamed/react/firebase/useDocumentSnapshots.js'
+import { useDocumentSnapshots } from './unnamed/react/firebase/useDocumentSnapshots.js'
 
 export function useSolutionProposals(solutionRequestId) {
   const queryRef = useMemo(
     () => getSolutionProposalsCollection(solutionRequestId),
     [solutionRequestId],
   )
-  return useDocumentSnapshot(queryRef)
+  return useDocumentSnapshots(queryRef)
 }
