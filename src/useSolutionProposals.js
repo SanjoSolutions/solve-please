c3c0636fc9fb9b5660553521fc8d77c65ec02a09
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2021-09-15 15:11:08 +0200

    Fixes repeated querying

--- src/useSolutionProposals.js
+++ src/useSolutionProposals.js
@@ -1,7 +1,11 @@
+import { useMemo } from 'react'
 import { getSolutionProposalsCollection } from './getSolutionProposalsCollection.js'
 import { useDocuments } from './unnamed/react/firebase/useDocuments.js'
 
 export function useSolutionProposals(solutionRequestId) {
-  const queryRef = getSolutionProposalsCollection(solutionRequestId)
+  const queryRef = useMemo(
+    () => getSolutionProposalsCollection(solutionRequestId),
+    [solutionRequestId]
+  )
   return useDocuments(queryRef)
 }
